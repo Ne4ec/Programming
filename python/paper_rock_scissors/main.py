d404527eeab0484be054e9ab1daffb8ef9d49a18
NE4kuma <ne4ec@proton.me>
2024-11-22 02:03:17 +0100

    now PJ

---
+++ python/paper_rock_scissors/main.py
@@ -0,0 +1,32 @@
+#!/bin/python
+
+import random
+
+def play():
+    computer_turn = random.chice (['r', 'p', 's'])
+    user_turn = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors")
+
+
+if computer_turn == user_turn:
+    print('its tie!')
+    print('Try another play.')
+    
+def win():
+    if (computer_turn == 'r' and user_turn == 'p'):
+        print("You lost!")
+    elif 
+
+    # r < p, p > s, s > r
+
+
+
+    
+###
+def play(chice):
+    computer_chice = random.chice(x)
+    print('The Computer has given thair move...')
+    player_turn = input('Your turn: '
+    
+    if player_turn == x:
+        print('You and the computer have been choiced the same. Try another Move')
+    elif player_turn 
