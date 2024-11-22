b73836d9143080112ddade95a481a974c684ff3e
NE4kuma <ne4ec@proton.me>
2024-11-22 18:07:33 +0100

    .

--- python/paper_rock_scissors/main.py
+++ python/paper_rock_scissors/main.py
@@ -2,31 +2,26 @@
 
 import random
 
-def play():
-    computer_turn = random.chice (['r', 'p', 's'])
-    user_turn = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors")
+attempt = 0
 
+computer_turn = random.choice (['r', 'p', 's'])
+user_turn = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors \n:")
+    
+ while True: #attempt <= 3:
+    if (computer_turn == 'p') and (user_turn == 'r'):
+        print('You lost!')
+        print('Computer have paper!')
+    elif (computer_turn == 's') and (user_turn == 'p'):             print('You lost!')
+        print('Computer have scissors!')
+    else:
+        print('You Won')
+        print(f'You have to win {attempt} times')
 
 if computer_turn == user_turn:
     print('its tie!')
     print('Try another play.')
-    
-def win():
-    if (computer_turn == 'r' and user_turn == 'p'):
-        print("You lost!")
-    elif 
+
+print(0)
 
     # r < p, p > s, s > r
 
-
-
-    
-###
-def play(chice):
-    computer_chice = random.chice(x)
-    print('The Computer has given thair move...')
-    player_turn = input('Your turn: '
-    
-    if player_turn == x:
-        print('You and the computer have been choiced the same. Try another Move')
-    elif player_turn 
