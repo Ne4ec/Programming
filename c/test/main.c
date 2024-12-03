38c509cd45c8f29e3e42eaa5e50fbd5ce96c3cf9
NE4kuma <ne4ec@proton.me>
2024-12-03 01:45:06 +0100

    Arguments in c

--- c/test/main.c
+++ c/test/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h> // Bibliothek für Input/Output
 
 int main() {
+	
+	//char name[3] = "Bob";
+	//int age = 27;
+	//float money = 3.4f;
+	//printf("%s is %d years old, he has %f€", name, age, money);
+	
+	//int age = 19;
+	//char name[] = "Kai";
 
-	char name[3] = "Bob";
-	int age = 27;
-	float money = 3.4f;
-	printf("%s is %d years old, he has %f€", name, age, money);
+	//if (age >= 18) {
+	//	printf("%s can enter!", name);
+	//} else if (name == "Kai"){
+	//	printf("Because you %s, you can enter", name);
+	//} else {
+	//	printf("%s can't enter!", name);
+	//}
 
-	return 0;
 	
 }
